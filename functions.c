ee6ac01f05df0c99dba2133481a44497ba1f31d9
Celio Maroja <c3rd.dev@gmail.com>
2023-02-18 18:32:11 -0300

    getting nubmers dynamically

--- functions.c
+++ functions.c
@@ -4,7 +4,16 @@ int add_two_ints(int a, int b); //declared function here to use before the funct
 
 int main(void)
 {
-    int result = add_two_ints(58, 36);
+
+    int a;
+    int b;
+
+    printf("Type in the first number: ");
+    scanf("%d", &a);
+    printf("Type in the second number: ");
+    scanf("%d", &b);
+
+    int result = add_two_ints(a, b);
     printf("O resultado é: %i\n", result);
 }
 
